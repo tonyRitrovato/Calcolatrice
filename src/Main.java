45b65bb6565da22314b601210cfab736ee98542c
Tony Ritrovato <antonioritrovato17@gmail.com>
2022-11-26 18:27:47 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
+
 
 
 public class Main {
 
 	public static void main(String args[]) {
-		try {
-			Calcolatrice c = new Calcolatrice();
-		}
-		catch(Exception e) {
-			JFrame error = new JFrame();
-			error.getContentPane().add(new JLabel(e.toString()));
-			error.setSize(200,200);
-			error.setVisible(true);
-		}
+		Calcolatrice c = new Calcolatrice();
 	}
 }
