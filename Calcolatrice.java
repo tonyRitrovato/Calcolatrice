c5a9a029fe433702f579c8e866599274a6db439a
Tony Ritrovato <antonioritrovato17@gmail.com>
2022-11-26 18:27:58 +0100

    first commit

--- Calcolatrice.java
+++
@@ -1,8 +0,0 @@
-class Calcolatrice {
-
-    public Calcolatrice() {
-        super("calcolatrice binaria");
-        setSize(300,800);
-        setVisible(true);
-    }
-}
--- Main.java
+++
@@ -1,6 +0,0 @@
-class Main {
-
-    public static void main() {
-        Calcolatrice c = new Calcolatrice();
-    }
-}
